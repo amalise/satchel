b31ed55ffa787e668d445b838b4ca989978b07b8
Karl Mariger <karl.mariger@gmail.com>
2022-04-03 20:02:06 +0000

    Initial blank Wagtail setup.

---
+++ satchel/satchel/settings/production.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = True
+
+
+# Security Settings
+# https://tonyteaches.tech/django-production-server-settings/
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 300  # 5 minutes
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+
+# Local Settings
+try:
+    from .local_settings import *
+except ImportError:
+    pass
